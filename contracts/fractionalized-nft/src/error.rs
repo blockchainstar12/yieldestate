c0b76a357db546042b371b239866c12f855c11ac
blockchainstar18 <blockchainstar18@gmail.com>
2024-03-28 09:00:54 -0700

    first commit

---
+++ contracts/fractionalized-nft/src/error.rs
@@ -0,0 +1,36 @@
+use cosmwasm_std::StdError;
+use cw_ownable::OwnershipError;
+use thiserror::Error;
+
+#[derive(Error, Debug, PartialEq)]
+pub enum ContractError {
+    #[error(transparent)]
+    Std(#[from] StdError),
+
+    #[error(transparent)]
+    Ownership(#[from] OwnershipError),
+
+    #[error(transparent)]
+    Version(#[from] cw2::VersionError),
+
+    #[error("Insufficient deposit amount")]
+    Insufficient {},
+
+    #[error("Invalid deposit token")]
+    InvalidDenom {},
+
+    #[error("Purchase overflow")]
+    PurchaseOverflow {},
+
+    #[error("token_id already claimed")]
+    Claimed {},
+
+    #[error("Cannot set approval that is already expired")]
+    Expired {},
+
+    #[error("Approval not found for: {spender}")]
+    ApprovalNotFound { spender: String },
+
+    #[error("No withdraw address set")]
+    NoWithdrawAddress {},
+}
